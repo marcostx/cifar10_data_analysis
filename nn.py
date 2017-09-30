ad418a01066c8b4f77d65927120367b9e68f2164
ecclesiedei <ecclesiedei@gmail.com>
2017-09-30 19:48:13 -0300

    rede de 1 layer funcionando

--- nn.py
+++ nn.py
@@ -19,19 +19,63 @@ import os
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 import tensorflow as tf
 
+
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from keras.datasets import cifar10
 from sklearn.model_selection import cross_val_score
+from util import *
 import time
 from random import shuffle
 
 seed = 44
 rng = np.random.RandomState(seed)
 
+
+# parameters
+input_shape = 32*32
+hidden_1 	= 128
+classes     = 10 
+epochs 		= 150
+batch_size  = 50
+lr 			= 0.001
+
+
+def preprocessing(X):
+	X_prec = []
+	print(X.shape)
+
+	for item in X:
+		gray_image = cv2.cvtColor(item, cv2.COLOR_BGR2GRAY)
+
+		X_prec.append(gray_image)
+
+
+	X_prec = np.array(X_prec)
+	X_prec = X_prec.reshape((-1,32 * 32))
+
+	return X_prec
+
+
+def batch_creator(train_x,batch_size, dataset_length, y):
+    """Create batch with random samples and return appropriate format"""
+    
+    batch_mask = rng.choice(dataset_length, batch_size)
+    
+    batch_x = train_x[[batch_mask]].reshape(-1, input_shape)
+    
+    batch_x = preproc(batch_x)
+    
+    
+    batch_y = y[[batch_mask]]
+    batch_y = oneHotEncoding(batch_y)
+        
+    return batch_x, batch_y
 
 def third_task(x_train,x_test,y_train,y_test):
 	"""
@@ -52,14 +96,9 @@ def third_task(x_train,x_test,y_train,y_test):
 	X_ = np.concatenate((x_train,x_test))
 	y_ = np.concatenate((y_train,y_test))
 	y_ = [item[0] for item in y_]
-
-	# parameters
-	input_shape = 32*32
-	hidden_1 	= 128
-	classes     = 10 
-	epochs 		= 150
-	batch_size  = 50
-	lr 			= 0.001
+	y_ = np.array(y_)
+	
+	X_ = preprocessing(X_)
 
 
 	# inputs placeholders
@@ -84,10 +123,9 @@ def third_task(x_train,x_test,y_train,y_test):
     
     # loss definition
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=out_layer, labels=y))
-	optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
-
 	# minimize the cost using SGD
-	optimizer.minimize(cost)
+	optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr).minimize(cost)
+
 
 	# init the variables
 	init = tf.global_variables_initializer()
@@ -95,36 +133,53 @@ def third_task(x_train,x_test,y_train,y_test):
 	number = 0
 	start = time.time()
 	for train_index, test_index in skf.split(X_, y_):
-	    print(y_)
-
-	    X_train, X_test = X_[train_index], X_[test_index]
-	    y_train, y_test = y_[train_index], y_[test_index]
+		
+		X_train, X_test = X_[train_index], X_[train_index]
+		y_train_, y_test_ = y_[train_index], y_[test_index]
 	    
-	    train_x = np.stack(X_train)
-	    print("Fold : ", number)
+		train_x = np.stack(X_train)
+		print("Fold : ", number)
+		with tf.Session() as sess:
+			# initializing vars
+			sess.run(init)
+	        
+	        # training
+			for epoch in range(epochs):
+				avg_cost = 0
+
+				total_batch = int(len(train_x)/batch_size)
+				for i in range(total_batch):
+					batch_x, batch_y = batch_creator(train_x,batch_size,train_x.shape[0], y_train_)
+					_, cost_ = sess.run([optimizer, cost], feed_dict = {x: batch_x, y: batch_y})
+	                
+					avg_cost += cost_ / total_batch
+	                
+				print "Epoch:", (epoch+1), "cost =", "{:.10f}".format(avg_cost)
+	        
+			print "\nTraining complete!"
+	    
+
+	        # find predictions on val set
+	        predict = tf.argmax(pred, 1)
+	        predictions = predict.eval({x: X_test.reshape(-1, classes)})
+	        
+	        print("accuracy : ", accuracy_score(y_test, predictions ) )
+	        print("precision : ", precision_score(y_test, predictions, average='weighted' ) )
+	        print("recall : ", recall_score(y_test, predictions,average='weighted' ) )
+	        print("f1 : ", f1_score(y_test, predictions,average='weighted' ) )
+	        print("\n")
+    
+    	number+=1
 
 	print("it took", time.time() - start, "seconds.")
-
-
-
-
-
-
-
-
-
-
-
-	
-
 	
 
 if __name__ == '__main__':
 	#firt_task()
 	# loading splitted dataset 
 	(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-	x_train = x_train.reshape((-1,32 * 32 * 3))
-	x_test = x_test.reshape((-1,32 * 32 * 3))
+	#x_train = x_train.reshape((-1,32 * 32 * 3))
+	#x_test = x_test.reshape((-1,32 * 32 * 3))
 
 	third_task(x_train,x_test,y_train,y_test)
 
